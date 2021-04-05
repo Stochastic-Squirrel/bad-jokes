09a22e5a2e8ce585e6bdcde4f0c19029a8896147
Sean Soutar <s1959963@ed.ac.uk>
2021-04-05 18:53:21 +0100

    updated pre-commit yaml. Need to add in a setup.cfg

--- bad_jokes/joke_generator.py
+++ bad_jokes/joke_generator.py
@@ -4,10 +4,12 @@ bad_jokes: joke_generator module
 This module contains classes responsible for generating jokes.
 """
 from typing import List
+
+
 class JokeGenerator:
-    """
-    This class generates a joke by calling underlying joke models and providing input where necessary.
-    """
+    """This class generates a joke by calling underlying joke models and
+    providing input where necessary."""
+
     def gen_jokes(self, n: int = 1) -> List[str]:
         jokes = [f"TEST JOKE:{i}" for i in range(n)]
         return jokes
